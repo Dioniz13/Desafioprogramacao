5afe20d3c05ea264f37d8b311e0bb7cf40811e60
Dioniz13 <ddferreira2022@outlook.com>
2025-10-01 20:48:05 -0300

    concatenacaodojogosecreto

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,9 +1,10 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 }else {
-    console.log('Que pena! Tente novamente');
+   alert('Que pena! Tente novamente');
 }
