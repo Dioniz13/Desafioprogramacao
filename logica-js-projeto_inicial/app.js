a4636f198b64704e929c20abf64d1588302aebcd
Dioniz13 <ddferreira2022@outlook.com>
2025-10-02 16:59:36 -0300

    contagem de tentativas

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,20 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
 let numeroSecreto = 9;
 console.log(numeroSecreto);
-let chute
+let chute;
+let tentativas = 1;
 
 //enquanto chute não for igual ao n.s.
 while(chute != numeroSecreto) {
-    chute = prompt('Digite um número entre 0 e 10');
+        chute = prompt('Digite um número entre 0 e 10');
    // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+        alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
     } else {
         if (numeroSecreto > chute) {
         alert(`O número secreto é maior que ${chute}`);
         } else {
         alert(`O número secreto é menor que ${chute}`);
         }
+        tentativas++;
 }
 
 }
